4a6b6485c4ccd5fc8b8446578e73e243898b9f5c
sanch0y <mahmudul.sanchoy@gmail.com>
2020-08-17 21:38:22 +0600

    Changed book model.

--- src/main/java/com/kishwar/librarybookingapp/ui/controller/BookController.java
+++ src/main/java/com/kishwar/librarybookingapp/ui/controller/BookController.java
@@ -3,6 +3,7 @@ package com.kishwar.librarybookingapp.ui.controller;
 import com.kishwar.librarybookingapp.service.BookService;
 import com.kishwar.librarybookingapp.ui.model.BookModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class BookController {
     @Autowired
     BookService bookService;
 
+    @CrossOrigin
     @GetMapping
     public List<BookModel> getAllBooks(){
         return bookService.getAllBooks();
