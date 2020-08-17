71c2e6c291e7445d7a2e20aec5dc93265a745243
KishwarMaheen <34351287+KishwarMaheen@users.noreply.github.com>
2020-08-17 22:56:34 +0600

    Creating booking API

--- src/main/java/com/kishwar/librarybookingapp/service/BookingService.java
+++ src/main/java/com/kishwar/librarybookingapp/service/BookingService.java
@@ -1,16 +1,22 @@
 package com.kishwar.librarybookingapp.service;
 
+import com.kishwar.librarybookingapp.io.repository.BookRepository;
 import com.kishwar.librarybookingapp.io.repository.BookingRepository;
+import com.kishwar.librarybookingapp.io.repository.UserRepository;
+import com.kishwar.librarybookingapp.ui.model.BookModel;
+import com.kishwar.librarybookingapp.ui.model.BookingModel;
+import com.kishwar.librarybookingapp.ui.model.BookingRequestModel;
+import com.kishwar.librarybookingapp.ui.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
 
 @Service
 public class BookingService {
 
-    @Autowired
-    BookingRepository bookingRepository;
-
-    public void booking(String bookName){
-
-    }
+    private static long count = 1;
 }
+
+
